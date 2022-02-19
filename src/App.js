7e681ce1cc1c40b907d218a32f77e31e6d174c8e
e.bedoya <esteban.bedoya.ext@kavak.com>
2022-02-19 16:35:01 -0500

    add events handlers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  state = {
+    currentEvent: '----'
+  }
+
+  update = (e) => {
+    this.setState({currentEvent: e.type})
+  }
   render() {
     return(
-      <h1>Hello world</h1>
+      <React.Fragment>
+        <textarea 
+          cols="30" 
+          rows="10" 
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onPaste={this.update}
+          onCut={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+        />
+        <h1>{this.state.currentEvent}</h1>
+      </React.Fragment>
     )
   }
 }

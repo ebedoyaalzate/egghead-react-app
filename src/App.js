d5a80e95ad76d8b9ab708614c7e3085756fa3423
e.bedoya <esteban.bedoya.ext@kavak.com>
2022-02-19 16:03:11 -0500

    add state and binding methods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  state = {
+    txt: 'this is the state text',
+    cat: 0
+  }
+
+  update = (e) => {
+    this.setState({txt: e.target.value})
+  }
+
   render() {
     return(
-      <h1>Hello world</h1>
+      <React.Fragment>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+        <input type="text" onChange={this.update}></input>
+      </React.Fragment>
     )
   }
 }
